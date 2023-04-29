e77469202392699cf919d1a545c6f38d45989871
toshi <toshi@toshi.click>
2023-04-30 08:02:42 +0900

    ブログ名修正

--- blog/src/consts.ts
+++ blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global posts in this file.
 // You can import this posts from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'しがないITインフラエンジニアの雑記';
+export const SITE_DESCRIPTION = 'しがないITインフラエンジニアの雑記';
